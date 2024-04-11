aaa86b779d747c2b41a51bfb246703f978d291bc
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-04-11 19:52:53 +0200

    Modified files
    added filter, search button and moreButton to display more actors on the home page

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Web-Technology-Project
+
+Group: 2B4\
+-Golache Denisa-Ioana\
+-Maxim Casiana
---
+++ moreButton.js
@@ -0,0 +1,9 @@
+document.getElementById("moreButton").addEventListener("click", function(event) {
+    event.preventDefault();
+    var actorsContainer = document.getElementById("actorsTable").getElementsByClassName("actorsLine")[0];
+    var newActors = '';
+    for (var i = 0; i < 9; i++) {
+        newActors += '<a href="actorProfile.html"><div class="actorCircle"></div></a>';
+    }
+    actorsContainer.innerHTML += newActors;
+});
