8e9b6ecdc6dd7b7f041dcfe42d191e7c259390e4
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-05-18 18:28:07 +0300

    update structure

--- js/moreButton.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("moreButton").addEventListener("click", function(event) {
-    event.preventDefault();
-    var actorsContainer = document.getElementById("actorsTable").getElementsByClassName("actorsLine")[0];
-    var newActors = '';
-    for (var i = 0; i < 50; i++) {
-        newActors += '<a href="../html/actorProfile.html"><div class="actorCircle"></div></a>';
-    }
-    actorsContainer.innerHTML += newActors;
-});
