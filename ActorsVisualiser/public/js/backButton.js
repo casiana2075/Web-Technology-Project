80336bfe3c379911d569f036c95cfc377f420283
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-06-20 03:45:05 +0200

    deleted

--- ActorsVisualiser/public/js/backButton.js
+++
@@ -1,4 +0,0 @@
-// JavaScript code to make the back button work
-document.getElementById('backButton').addEventListener('click', function() {
-    window.history.back(); // Navigate back to the previous page
-});
