194c78635c47ac4bfb273ea3b1622b83031529b7
denisagolache <126023804+denisagolache@users.noreply.github.com>
2024-06-20 19:47:12 +0200

    populated db with awardsInfo + login&register functionality

--- ActorsVisualiser/server/db.js
+++ ActorsVisualiser/server/db.js
@@ -1,4 +1,7 @@
 const pkg = require('pg');
+const fs = require('fs');
+const csv = require('csv-parser');
+
 const { Pool } = pkg;
 
 const pool = new Pool({
@@ -7,6 +10,57 @@ const pool = new Pool({
     password: 'STUDENT',
     database: 'AcVisDb',
     port: 5432,
+});
+
+function createTableIfNotExists() {
+    const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS "awardsInfo" (
+        year TEXT,
+        category TEXT,
+        full_name TEXT,
+        show TEXT,
+        won BOOLEAN
+    );`;
+
+    pool.query(createTableQuery, (err, res) => {
+        if (err) {
+            console.error('Error creating table', err.stack);
+        } else {
+            console.log('Table created or already exists');
+            populateDatabase();
+        }
+    });
+}
+
+function populateDatabase() {
+    const results = [];
+
+    fs.createReadStream('screen_actor_guild_awards.csv')
+        .pipe(csv())
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            results.forEach(row => {
+                const query = {
+                    text: 'INSERT INTO "awardsInfo" (year, category, full_name, show, won) VALUES ($1, $2, $3, $4, $5)',
+                    values: [row.year, row.category, row.full_name, row.show, row.won === 'true']
+                };
+
+                pool.query(query, (err, res) => {
+                    if (err) {
+                        console.error('Error executing query', err.stack);
+                    } else {
+                 //       console.log('Row inserted');
+                    }
+                });
+            });
+        });
+}
+
+pool.connect((err, client, done) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+    createTableIfNotExists();
+    done();
 });
 
 module.exports = pool;
