ddbe33a2cddd2144cc5f9dfcc47a0f6dbba4c3c7
Casiana <88460309+casiana2075@users.noreply.github.com>
2024-06-25 03:14:54 +0300

    reupload

--- ActorsVisualiser/client/public/js/moreButton.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('moreButton').addEventListener('click', function(event) {
-    event.preventDefault();
-    currentPage++;  
-    fetchAndDisplayActors(currentPage); 
-});
--- ActorsVisualiser/client/public/js/welcome.js
+++
@@ -1,4 +0,0 @@
-if (document.cookie.includes('username' || 'admin')) {
-    window.location.href = 'homePage.html';
-    window.alert("You are already logged in. Please log out to log in as an user.");
-}
